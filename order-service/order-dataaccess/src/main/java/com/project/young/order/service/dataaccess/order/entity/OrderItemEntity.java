e3d36333845896e7b7cdd17e5d6dbf12d1546895
Young wook Jeon <ela87@naver.com>
2022-06-13 16:58:31 +0900

    Add dependencies about accessing data

---
+++ order-service/order-dataaccess/src/main/java/com/project/young/order/service/dataaccess/order/entity/OrderItemEntity.java
@@ -0,0 +1,47 @@
+package com.project.young.order.service.dataaccess.order.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Objects;
+import java.util.UUID;
+
+@Table(name = "order_items") @Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@IdClass(OrderItemEntityId.class)
+public class OrderItemEntity {
+
+    @Id
+    private Long id;
+
+    @Id
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "ORDER_ID")
+    private OrderEntity order;
+
+    private UUID productId;
+
+    private BigDecimal price;
+
+    private Integer quantity;
+
+    private BigDecimal subTotal;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderItemEntity that = (OrderItemEntity) o;
+        return id.equals(that.id) && order.equals(that.order);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, order);
+    }
+}
