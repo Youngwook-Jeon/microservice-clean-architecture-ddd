408e7f394c01c23c5dee913e324e1d19bb91a63f
Young wook Jeon <ela87@naver.com>
2022-06-21 19:00:19 +0900

    Add our customer-service module

---
+++ customer-service/src/main/resources/init-data.sql
@@ -0,0 +1,5 @@
+INSERT INTO customer.customers(id, username, first_name, last_name)
+    VALUES ('67e74011-99b5-4f06-9ee4-14949969251d', 'user_1', 'First', 'User');
+
+INSERT INTO customer.customers(id, username, first_name, last_name)
+    VALUES ('53b33940-425b-4f22-9810-3364dfccfbbe', 'user_2', 'Second', 'User');
