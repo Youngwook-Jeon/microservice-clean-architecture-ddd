f6ba0e0088d897e7f6b3f13d559f2bf76fa22684
Young wook Jeon <ela87@naver.com>
2022-06-28 22:07:12 +0900

    Add a helper class to implement the message listener class of payment

---
+++ payment-service/payment-domain/payment-application-service/src/main/java/com/project/young/payment/service/domain/PaymentRequestMessageListenerImpl.java
@@ -0,0 +1,21 @@
+package com.project.young.payment.service.domain;
+
+import com.project.young.payment.service.domain.dto.PaymentRequest;
+import com.project.young.payment.service.domain.ports.input.message.listener.PaymentRequestMessageListener;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class PaymentRequestMessageListenerImpl implements PaymentRequestMessageListener {
+
+    @Override
+    public void completePayment(PaymentRequest paymentRequest) {
+
+    }
+
+    @Override
+    public void cancelPayment(PaymentRequest paymentRequest) {
+
+    }
+}
--- payment-service/payment-domain/payment-application-service/src/main/java/com/project/young/payment/service/domain/mapper/PaymentDataMapper.java
+++ payment-service/payment-domain/payment-application-service/src/main/java/com/project/young/payment/service/domain/mapper/PaymentDataMapper.java
@@ -1,7 +1,22 @@
 package com.project.young.payment.service.domain.mapper;
 
+import com.project.young.domain.valueobject.CustomerId;
+import com.project.young.domain.valueobject.Money;
+import com.project.young.domain.valueobject.OrderId;
+import com.project.young.payment.service.domain.dto.PaymentRequest;
+import com.project.young.payment.service.domain.entity.Payment;
 import org.springframework.stereotype.Component;
+
+import java.util.UUID;
 
 @Component
 public class PaymentDataMapper {
+
+    public Payment paymentRequestModelToPayment(PaymentRequest paymentRequest) {
+        return Payment.builder()
+                .orderId(new OrderId(UUID.fromString(paymentRequest.getOrderId())))
+                .customerId(new CustomerId(UUID.fromString(paymentRequest.getCustomerId())))
+                .price(new Money(paymentRequest.getPrice()))
+                .build();
+    }
 }
