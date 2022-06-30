3ad74411dd1564f6c994a27b5ccfd28f43f1b5db
Young wook Jeon <ela87@naver.com>
2022-06-30 20:16:48 +0900

    Implement payment event listener class

--- payment-service/payment-domain/payment-application-service/src/main/java/com/project/young/payment/service/domain/PaymentRequestMessageListenerImpl.java
+++ payment-service/payment-domain/payment-application-service/src/main/java/com/project/young/payment/service/domain/PaymentRequestMessageListenerImpl.java
@@ -1,7 +1,14 @@
 package com.project.young.payment.service.domain;
 
 import com.project.young.payment.service.domain.dto.PaymentRequest;
+import com.project.young.payment.service.domain.event.PaymentCancelledEvent;
+import com.project.young.payment.service.domain.event.PaymentCompletedEvent;
+import com.project.young.payment.service.domain.event.PaymentEvent;
+import com.project.young.payment.service.domain.event.PaymentFailedEvent;
 import com.project.young.payment.service.domain.ports.input.message.listener.PaymentRequestMessageListener;
+import com.project.young.payment.service.domain.ports.output.message.publisher.PaymentCancelledMessagePublisher;
+import com.project.young.payment.service.domain.ports.output.message.publisher.PaymentCompletedMessagePublisher;
+import com.project.young.payment.service.domain.ports.output.message.publisher.PaymentFailedMessagePublisher;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -9,13 +16,44 @@ import org.springframework.stereotype.Service;
 @Service
 public class PaymentRequestMessageListenerImpl implements PaymentRequestMessageListener {
 
+    private final PaymentRequestHelper paymentRequestHelper;
+    private final PaymentCompletedMessagePublisher paymentCompletedMessagePublisher;
+    private final PaymentCancelledMessagePublisher paymentCancelledMessagePublisher;
+    private final PaymentFailedMessagePublisher paymentFailedMessagePublisher;
+
+    public PaymentRequestMessageListenerImpl(
+            PaymentRequestHelper paymentRequestHelper,
+            PaymentCompletedMessagePublisher paymentCompletedMessagePublisher,
+            PaymentCancelledMessagePublisher paymentCancelledMessagePublisher,
+            PaymentFailedMessagePublisher paymentFailedMessagePublisher) {
+        this.paymentRequestHelper = paymentRequestHelper;
+        this.paymentCompletedMessagePublisher = paymentCompletedMessagePublisher;
+        this.paymentCancelledMessagePublisher = paymentCancelledMessagePublisher;
+        this.paymentFailedMessagePublisher = paymentFailedMessagePublisher;
+    }
+
     @Override
     public void completePayment(PaymentRequest paymentRequest) {
-
+        PaymentEvent paymentEvent = paymentRequestHelper.persistPayment(paymentRequest);
+        fireEvent(paymentEvent);
     }
 
     @Override
     public void cancelPayment(PaymentRequest paymentRequest) {
+        PaymentEvent paymentEvent = paymentRequestHelper.persistCancelPayment(paymentRequest);
+        fireEvent(paymentEvent);
+    }
 
+    private void fireEvent(PaymentEvent paymentEvent) {
+        log.info("Publishing payment event with payment id: {} and order id: {}",
+                paymentEvent.getPayment().getId().getValue(),
+                paymentEvent.getPayment().getOrderId().getValue());
+        if (paymentEvent instanceof PaymentCompletedEvent) {
+            paymentCompletedMessagePublisher.publish((PaymentCompletedEvent) paymentEvent);
+        } else if (paymentEvent instanceof PaymentCancelledEvent) {
+            paymentCancelledMessagePublisher.publish((PaymentCancelledEvent) paymentEvent);
+        } else if (paymentEvent instanceof PaymentFailedEvent) {
+            paymentFailedMessagePublisher.publish((PaymentFailedEvent) paymentEvent);
+        }
     }
 }
