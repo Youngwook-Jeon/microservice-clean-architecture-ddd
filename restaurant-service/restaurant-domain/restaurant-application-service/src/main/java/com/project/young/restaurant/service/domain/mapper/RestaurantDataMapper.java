45ad5d1121c249611e778465a79804ad5760169e
Young wook Jeon <ela87@naver.com>
2022-08-13 22:08:14 +0900

    Implement input ports for the Restaurant service

--- restaurant-service/restaurant-domain/restaurant-application-service/src/main/java/com/project/young/restaurant/service/domain/mapper/RestaurantDataMapper.java
+++ restaurant-service/restaurant-domain/restaurant-application-service/src/main/java/com/project/young/restaurant/service/domain/mapper/RestaurantDataMapper.java
@@ -1,28 +1,39 @@
 package com.project.young.restaurant.service.domain.mapper;
 
+import com.project.young.domain.valueobject.Money;
+import com.project.young.domain.valueobject.OrderId;
+import com.project.young.domain.valueobject.OrderStatus;
+import com.project.young.domain.valueobject.RestaurantId;
+import com.project.young.restaurant.service.domain.dto.RestaurantApprovalRequest;
+import com.project.young.restaurant.service.domain.entity.OrderDetail;
+import com.project.young.restaurant.service.domain.entity.Product;
+import com.project.young.restaurant.service.domain.entity.Restaurant;
 import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Component
 public class RestaurantDataMapper {
 
-//    public Restaurant restaurantApprovalRequestToRestaurant(RestaurantApprovalRequest
-//                                                                    restaurantApprovalRequest) {
-//        return Restaurant.builder()
-//                .restaurantId(new RestaurantId(UUID.fromString(restaurantApprovalRequest.getRestaurantId())))
-//                .orderDetail(OrderDetail.builder()
-//                        .orderId(new OrderId(UUID.fromString(restaurantApprovalRequest.getOrderId())))
-//                        .products(restaurantApprovalRequest.getProducts().stream().map(
-//                                        product -> Product.builder()
-//                                                .productId(product.getId())
-//                                                .quantity(product.getQuantity())
-//                                                .build())
-//                                .collect(Collectors.toList()))
-//                        .totalAmount(new Money(restaurantApprovalRequest.getPrice()))
-//                        .orderStatus(OrderStatus.valueOf(restaurantApprovalRequest.getRestaurantOrderStatus().name()))
-//                        .build())
-//                .build();
-//    }
-//
+    public Restaurant restaurantApprovalRequestToRestaurant(RestaurantApprovalRequest
+                                                                    restaurantApprovalRequest) {
+        return Restaurant.builder()
+                .restaurantId(new RestaurantId(UUID.fromString(restaurantApprovalRequest.getRestaurantId())))
+                .orderDetail(OrderDetail.builder()
+                        .orderId(new OrderId(UUID.fromString(restaurantApprovalRequest.getOrderId())))
+                        .products(restaurantApprovalRequest.getProducts().stream().map(
+                                        product -> Product.builder()
+                                                .productId(product.getId())
+                                                .quantity(product.getQuantity())
+                                                .build())
+                                .collect(Collectors.toList()))
+                        .totalAmount(new Money(restaurantApprovalRequest.getPrice()))
+                        .orderStatus(OrderStatus.valueOf(restaurantApprovalRequest.getRestaurantOrderStatus().name()))
+                        .build())
+                .build();
+    }
+
 //    public OrderEventPayload
 //    orderApprovalEventToOrderEventPayload(OrderApprovalEvent orderApprovalEvent) {
 //        return OrderEventPayload.builder()
